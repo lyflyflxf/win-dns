7d0d47ba51212c0197de109b64bfa6917a5f2e9c
Ethan <lyflyflxf@gmail.com>
2018-01-10 22:05:14 +0800

    Reformatting, add docstrings, bug fix

--- lid.py
+++ lid.py
@@ -13,30 +13,34 @@ Example:
 
 Start the file will lead to a simple CMD interface.
 
-Todo:
-
 """
 import os
 import wmi
 import subprocess
-
 import hosts_update
 
 wmi = wmi.WMI()
 
+
 def url2domain(url):
+    """Process url into domain suitable for ping in WMI
+
+    Args:
+        url (str):
+
+    Returns:
+        domain string
+    """
     import validators
     from urllib.parse import urlparse
 
-    error = ValueError(url)
+    err = ValueError(url)
     error_class = validators.utils.ValidationFailure
-
-    domain=''
-
+    domain = ''
     try:
         url = urlparse(url)
-    except:
-        raise error
+    except Exception:
+        raise err
     else:
         if url.netloc != '':
             domain = url.netloc
@@ -44,30 +48,27 @@ def url2domain(url):
             domain = url.path
             if domain.find(r'/'):
                 domain = domain.split(r'/')[0]
-    # else:
-    #     raise error
-
     if isinstance(validators.domain(domain), error_class) and (
             isinstance(validators.ipv4(domain), error_class)):
-        raise error
+        raise err
     else:
         return domain
 
 
 def ping(url):
-    """Ping the url 3 times with maximal 255 TTL using windows ping command.
+    """Ping the url using WMI functionality.
 
        Args:
            url (str):
 
        Returns:
-
-       """
+           result parsed into dict
+    """
     result = wmi.Win32_PingStatus(Address=url2domain(url))[0]
-
     return {'ip': result.ProtocolAddress,
             'time': result.ResponseTime,
             'domain': url, }
+
 
 def update():
     hosts_update.update()
@@ -77,53 +78,54 @@ def purge():
     hosts_update.write()
 
 
-class Dns():
+class Dns:
     ali = "Ali"
     one = "One"
     tencent = "Tencent"
-    local = "Local"
+    local = "Acrylic"
     pabo = 'PaBo'
 
-    '''DNS地址必须为tuple，如果只有一个IP，则必须在IP后且括号内加上逗号，如('xxx',)'''
+    # DNS地址必须为tuple，如果只有一个IP，则必须在IP后且括号内加上逗号，如('xxx',)
     dict = {ali: ('223.5.5.5', '223.6.6.6'),
             one: ('114.215.126.16', '42.236.82.22'),
             tencent: ('119.29.29.29',),
             local: ('127.0.0.1', '223.6.6.6'),
-            pabo: ('123.207.164.150','140.143.87.229', ), }
+            pabo: ('123.207.164.150', '140.143.87.229',), }
 
     def __init__(self):
+        self.preferences = [self.pabo, self.tencent, ]  # preferred DNS servers
         for each in wmi.Win32_NetworkAdapterConfiguration(IPEnabled=True):
             if each.DefaultIPGateway is not None:
                 # self.index = each.Index
                 # self.ip = each.IPAddress[0]
                 self.server = each.DNSServerSearchOrder
-                self.now=each
+                self.now = each
                 return
 
-        self.server = '' #用wmi ping None貌似会陷入死循环
+        self.server = ''  # 用wmi ping None貌似会陷入死循环
         return
 
-    def set(self, i):
-        options=[self.local,self.pabo]
-
-        return_code= self.now.SetDNSServerSearchOrder(self.dict[options[i]])[0]
-
-        if return_code == 0:
-            return '修改成功'
-        else:
-            return '修改失败，错误代码：' + return_code
-
     def __repr__(self):
-        if self.server=='':
+        """Provide DNS information for print.
+
+        Returns:
+            string of DNS server IP
+        """
+        if self.server == '':
             return 'No Internet Connection'
-        for key,value in self.dict.items():
+        for key, value in self.dict.items():
             if self.server == value:
                 # print(key)
                 return key + ' DNS'
         return str(self.server)
 
     def refresh(self):
-        if self.__repr__() == 'Local':
+        """Refresh DNS pool for the system or Acrylic
+
+        Returns:
+            Result of refreshment
+        """
+        if self.__repr__() == self.local:
             subprocess.Popen(['D:\Program Files (x86)\Acrylic-Portable\AcrylicController.exe',
                               'PurgeAcrylicCacheDataSilently'])
         else:
@@ -134,17 +136,19 @@ class Dns():
                 return '刷新失败'
 
     def switch(self):
+        """Switch DNS among preferred ones.
+
+        Returns:
+            Result of action
         """
+        switches = [self.dict[x] for x in (self.preferences + [self.preferences[0]])]
 
-              Returns:
+        if self.server in switches:
+            server = switches[switches.index(self.server) + 1]
+        else:
+            server = switches[0]
 
-              """
-        l= [self.dict[x] for x in [self.tencent,self.pabo]]
-
-        if self.server in l:
-            l.pop(l.index(self.server))
-
-        return_code = self.now.SetDNSServerSearchOrder(l[len(l) - 1])[0]
+        return_code = self.now.SetDNSServerSearchOrder(server)[0]
         self.refresh()
         if return_code == 0:
             return '修改成功'
@@ -161,17 +165,20 @@ if __name__ == '__main__':
     5.更新hosts
     6.清空hosts
     7.编辑default.txt
+    8.查看hosts
     回车：退出
 '''
-
+    # 8.编辑Acrylic
     acrylic_dir = 'D:\Program Files (x86)\Acrylic-Portable\AcrylicConfiguration.ini'
     pre_msg = ''
 
+
     def ping_print(url):
+        """Format the result of ping"""
         try:
             out = ping(url2domain(url))
         except ValueError as e:
-            print('异常网址：',e)
+            print('异常网址：', e)
         else:
             print('\n正在 Ping %s，[%s]' % (out['domain'], out['ip']))
             for i in range(1, 4):
@@ -179,10 +186,11 @@ if __name__ == '__main__':
 
 
     def run(choice):
+        """Process choice from user"""
         if choice == '':
             exit()
         else:
-            choice=int(choice)
+            choice = int(choice)
 
         global pre_msg
 
@@ -211,25 +219,22 @@ if __name__ == '__main__':
             return
         elif choice == 7:
             hosts_update.edit()
-        # elif choice == 8:
+        elif choice == 8:
+            hosts_update.view()
         #     subprocess.Popen([r'C:\WINDOWS\system32\notepad.exe',acrylic_dir])
         # elif choice == 9:
         #     subprocess.Popen(['D:\Program Files (x86)\Acrylic-Portable\AcrylicController.exe',
         #                       'StartAcrylicServiceSilently'])
-
         else:
             raise ValueError('选项错误！')
-
 
     os.system('cls')
     while 1:
         dns = Dns()
-        print("%s 当前DNS为 %s，请选择："%(pre_msg, dns))
-
+        print("%s 当前DNS为 %s，请选择：" % (pre_msg, dns))
         try:
             run(input(input_message))
         except ValueError as error:
             print(error)
             os.system('pause')
-
         os.system('cls')
